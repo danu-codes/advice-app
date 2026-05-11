a290d709bce68bb50fe53a3eb1420a1c68473555
Sri_Danushan <mailtodanu07@gmail.com>
2026-05-12 00:12:04 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react';
 
 function App() {
-  const [advice, setAdvice]=useState("Please Click button to get advice");
+  const [advice, setAdvice] = useState("Please Click button to get advice");
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
 
-  async function getAdvice(){
-    const res=await fetch("https://api.adviceslip.com/advice");
-    const data=await res.json();
+  async function getAdvice() {
+    setLoading(true);
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+    setLoading(false);
   }
 
   return (
 
     <div>
-      <h3>{advice}</h3>
-      <button onClick={getAdvice}>Get Advice</button>
+      {loading ? <h3>Please wait, we are preparing your advice...</h3> : <h3>{advice}</h3>}
+      <button onClick={getAdvice} disabled={loading}>
+        {loading ? "Loading..." : "Get Advice"}
+      </button>
+      <p>You have read <b>{count}</b> pieces of advice</p>
     </div>
 
   )
